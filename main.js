267bbdf8ed2a1dbc8ab7d35614392ac6a37d0052
Maaz Hasan <hmaaz344@gmail.com>
2024-02-28 21:05:35 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var userName = ["Maaz", "Hasan", "Asif"];
+console.log("These users are verified:  ".concat(userName));
+var newUsers = ["Anabia", "Noreen", "Wania"];
+console.log("These user are new and are not verified in company: ".concat(newUsers));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let userName: string[] =["Maaz","Hasan","Asif"]
+console.log(`These users are verified:  ${userName}`)
+let newUsers: string[] =["Anabia","Noreen","Wania"]
+console.log(`These user are new and are not verified in company: ${newUsers}`)
